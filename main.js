48fad35cc7d2e9a50d842f3bfc8998020250bee7
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2018-11-08 21:13:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 
-const rectangle = document.createElement('div');
 let size = 100;
+let grow = true;
 
-rectangle.style.backgroundColor = 'green';
+document.body.style.height = '10000px';
+const div = document.createElement('div');
+document.body.appendChild(div);
 
-rectangle.style.width = size * 10 + 'vw';
-rectangle.style.height = size * 0.5 + 'vh';
-rectangle.style.position = 'fixed';
-rectangle.style.top = 0;
-rectangle.style.left = 0;
-// rectangle.style.width = '100px';
+div.style.position = 'fixed';
+div.style.top = 0;
+div.style.left = 0;
+div.style.width = '100%';
+div.style.height = size + 'px';
+div.style.backgroundColor = 'green';
 
-document.body.appendChild(rectangle);
-
-window.addEventListener('scroll', function () {
-    if (window.innerHeight == 0) {
-        size += 50;
-
+const changeHeight = function () {
+    if (size > window.innerHeight / 2) {
+        grow = false;
+        div.style.backgroundColor = 'red';
     }
-});
+    else if (size <= 0) {
+        grow = true;
+        div.style.backgroundColor = 'green';
+    }
+    if (grow == true) {
+        size += 10;
+        div.style.height = size + 'px'
+    }
+    else {
+        size -= 10;
+        div.style.height = size + 'px'
+    }
+}
+
+window.addEventListener('scroll', changeHeight);
 
 
