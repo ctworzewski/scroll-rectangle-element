b19e7d6ab1d1c8a730b77967b014239546151cca
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2018-11-08 21:33:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,35 +3,31 @@ let size = 100;
 let grow = true;
 
 document.body.style.height = '10000px';
-const div = document.createElement('div');
-document.body.appendChild(div);
+const rect = document.createElement('div');
+document.body.appendChild(rect);
 
-div.style.position = 'fixed';
-div.style.top = 0;
-div.style.left = 0;
-div.style.width = '100%';
-div.style.height = size + 'px';
-div.style.backgroundColor = 'green';
+rect.style.width = '100%';
+rect.style.height = size + 'px';
+rect.style.backgroundColor = 'green';
+rect.style.position = 'fixed';
+rect.style.left = 0;
+rect.style.top = 0;
 
-const changeHeight = function () {
+const growRect = function () {
     if (size > window.innerHeight / 2) {
         grow = false;
-        div.style.backgroundColor = 'red';
     }
     else if (size <= 0) {
         grow = true;
-        div.style.backgroundColor = 'green';
     }
     if (grow == true) {
         size += 10;
-        div.style.height = size + 'px'
+        rect.style.height = size + 'px';
     }
     else {
         size -= 10;
-        div.style.height = size + 'px'
+        rect.style.height = size + 'px';
     }
 }
 
-window.addEventListener('scroll', changeHeight);
-
-
+window.addEventListener('scroll', growRect);
